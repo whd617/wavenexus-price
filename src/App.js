8f8590d610d17a5c308467032741248873598446
Jonghyeon Choi <whd617@naver.com>
2024-07-19 14:00:23 +0900

    배포

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './routes/home';
+import Home from "./home";
 
 function App() {
   return (
