8073d0890ba9a46d909b14d1e864d345342c1a0e
Jonghyeon Choi <whd617@naver.com>
2024-07-19 14:19:27 +0900

    setup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
+        <Route path={"/"} element={<Home />}></Route>
       </Routes>
     </Router>
   );
